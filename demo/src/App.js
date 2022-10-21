578697829d5e3d76683f1b95e1c85c8b20facdea
huanghui <huanghui@huanghuideMacBook-Pro-2.local>
2022-10-21 20:55:23 +0800

    feat: init

---
+++ demo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Foo } from 'HPY-UI';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Foo title='hpy test' />
+      </header>
+    </div>
+  );
+}
+
+export default App;
